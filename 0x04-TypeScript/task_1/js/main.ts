7b37a068cfd051b0831ac03cf8fff029b4934ffb
Hanna Tariku <hannatariku1996@gmail.com>
2025-05-25 10:45:46 +0300

    student class implementation

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -35,5 +35,28 @@ const printTeacher: printTeacherFunction = (firstName , lastName ) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
 
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// class implementation
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
 
 
