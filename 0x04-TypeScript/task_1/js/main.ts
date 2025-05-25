785674d37e0fe8eca5ead24ce82589f84f8d174c
Hanna Tariku <hannatariku1996@gmail.com>
2025-05-25 10:07:49 +0300

    build a teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,9 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allows additional attributes without specifying the name of the attribute
+}
+
