e2c45a20aa00fb94a36f555a1958d85b95d880ad
Hanna Tariku <hannatariku1996@gmail.com>
2025-05-25 11:44:22 +0300

    create functions to specific employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -34,10 +34,22 @@ class Teacher implements TeacherInterface {
   }
 }
 
-const createEmployee =(salary: number | string): Director | Teacher => {
+const createEmployee = (salary: number | string): Director | Teacher => {
   if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   } else {
     return new Director();
+  }
+};
+
+const isDirector = (employee: Director | Teacher): employee is Director => {
+  return employee instanceof Director;
+};
+
+const executeWork = (employee: Director | Teacher): string => {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
   }
 };
